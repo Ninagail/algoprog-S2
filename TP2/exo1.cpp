829c3f18518b49bb74a0559fde6197cd13f751d3
Ninagail <119610609+Ninagail@users.noreply.github.com>
2023-02-24 17:56:26 +0100

    ex1

--- TP2/exo1.cpp
+++ TP2/exo1.cpp
@@ -1,21 +1,41 @@
-#include <QApplication>
-#include <time.h>
+/* Sans QT creator*/
 
-#include "tp2.h"
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <string.h>
+using namespace std;
 
-MainWindow* w = nullptr;
+typedef unsigned int uint ;
+typedef int int32 ;
+typedef unsigned short int uint8 ;
+typedef unsigned long int uint64 ;
 
-void selectionSort(Array& toSort){
-	// selectionSort
+
+void selectionSort(std::vector<int> & toSort){
+	for (uint i=0; i<toSort.size(); i++)
+    {
+        int min = i;
+        for (int j=i; j<toSort.size(); j++)
+        {
+            if (toSort[j] < toSort[min] ){
+                min=j;
+            }
+            if (min != i){
+            std::swap(toSort[i], toSort[min]);
+            }
+        }
+        
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-    uint elementCount=15; // number of elements to sort
-    MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
-    w = new TestMainWindow(selectionSort); // window which display the behavior of the sort algorithm
-    w->show();
+	std::vector<int> tab({8,1,5,9,11,3});
+    selectionSort(tab);
 
-	return a.exec();
+    for (int i=0; i<tab.size(); i++){
+        std::cout << tab[i] << endl;
+
+    }
 }

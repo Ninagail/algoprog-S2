c945a7f12a54c3ad155818f831b88194c6e6ac4d
Ninagail <119610609+Ninagail@users.noreply.github.com>
2023-03-17 18:04:33 +0100

    ex1 fini

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -10,46 +10,60 @@ using std::size_t;
 using std::string;
 // i*2+1 pour l'indice de l'enfant gauche
 // i*2+2 pour l'indice de l'enfant droit
-// (j-1)/2 pour avoir l'indice du parent
-int Heap::leftChild(int nodeIndex)
+// (i-1)/2 pour avoir l'indice du parent
+int Heap::leftChildIndex(int nodeIndex)
 {
-	int i=0;
-	this->nodeIndex =i;
-	this->left->nodeIndex = i*2+1;
-	
-    return 0;
+    return nodeIndex*2+1;
 }
 
 int Heap::rightChildIndex(int nodeIndex)
 {
-	int i=0;
-	this->nodeIndex =i;
-	this->right->nodeIndex= i*2+2;
-    return 0;
+    return nodeIndex*2+2;
 }
 
 void Heap::insertHeapNode(int heapSize, int value)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
-
 	int i = heapSize;
+	this->get(i)=value;
+	while(i>0 && this->get(i)>this->get((i-1)/2)){
+		swap(i,(i-1)/2);
+		i=(i-1)/2;
+	}
+
 }
 
 void Heap::heapify(int heapSize, int nodeIndex)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
-	if(this->)
 	int i_max = nodeIndex;
+
+	if(this->leftChildIndex(nodeIndex)<heapSize && this->get(leftChildIndex(nodeIndex))>this->get(i_max)){
+		i_max = this->leftChildIndex(nodeIndex);
+	}
+	if(this->rightChildIndex(nodeIndex)<heapSize && this->get(rightChildIndex(nodeIndex))>this->get(i_max)){
+		i_max = this->rightChildIndex(nodeIndex);
+	}
+	if(nodeIndex!=i_max){
+		swap(nodeIndex, i_max);
+		heapify(heapSize, i_max);
+	}
+	
 }
 
 void Heap::buildHeap(Array& numbers)
 {
-
+	for(int i=0; i<numbers.size(); i++){
+		insertHeapNode(i, numbers[i]);
+	}
 }
 
 void Heap::heapSort()
 {
-
+	for(int i = this->size()-1; i>0; i--){
+		swap(0,i);
+		heapify(i,0);
+	}
 }
 
 int main(int argc, char *argv[])
